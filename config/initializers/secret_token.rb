23176db4be8852ffd53a0410c2c1f36f15d552be
Bradley Spencer <rawwebdesign@gmail.com>
2012-05-08 21:20:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipcoupon::Application.config.secret_token = '32f826597ff10a51fd3049f343454431f70b2edc89520c24bcbe9d4dbe2adc8978b796776cc9d7b3e8f0cfb98f1fe259f67b0ce7ada793fb26eabddae0581897'
